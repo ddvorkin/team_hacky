6e798401b8108e86d1efbd501206c8021e866931
IsaacGluck <isaacbgluck@gmail.com>
2014-10-08 13:08:48 -0400

    woo

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 import google
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
