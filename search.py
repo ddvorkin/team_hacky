63a1eb5fca1c1c77952f3141e13b3172b783f5b8
angela97lin <angela97lin@gmail.com>
2014-10-13 02:41:33 -0400

    added countries in case we do where query

--- search.py
+++ search.py
@@ -3,6 +3,12 @@ from urllib import urlopen #to read files
 import google
 import re
 
+stream1 = open("countries.csv","r");
+read1 = stream1.read().replace("\n"," ");
+stream1.close();
+countries = {}
+countries = dict.fromkeys(read1.split(),0);
+#print countries
 
 #stream to read our first names file
 stream2 = open("firstnames.csv",'r')
@@ -11,7 +17,7 @@ stream2.close()
 
 firstDic = {}
 firstNames = read2.split() #all first names via our database
-firstDic = dict.fromkeys(firstNames, firstNames)
+firstDic = dict.fromkeys(firstNames, 0)
    
 #stream to read surnames file
 stream3 = open("surnames.csv",'r')
@@ -24,6 +30,7 @@ surnames = read3.split()
 #setting up surname dictionary
 for a in surnames:
     surnameDic[a.title()] = a.title()
+
 
 def search_who(question):
     g = google.search(question,num=5,stop=5)
@@ -47,7 +54,7 @@ def search_who(question):
         for peep in p:
             #print peep
             firstLast= peep.split()#"Angela Lin" -> ["Angela","Lin"]
-            if firstLast[0] in firstDic and firstLast[1] in surnameDic:
+            if firstLast[0] in firstDic.keys() and firstLast[1] in surnameDic.keys():
                 if peep not in people.keys():
                     people[peep] = 1;
                 else:
@@ -62,4 +69,4 @@ def search_who(question):
 
 
 if __name__ == "__main__":
-    search_who("who plays barney on how i met your mother")
+    search_who("who plays batman")
