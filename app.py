6f701d00f73ec972590daa4dfc25d3534c955814
IsaacGluck <isaacbgluck@gmail.com>
2014-10-13 13:41:52 -0400

    ok

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@app.route("/",methods=["GET","POST"])
+@app.route("/index",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+	if request.method=="GET":
+		return render_template("index.html")
+	else:
+		button = request.form['s']
+		if button == "search":
+			return render_template("result.html", result="searched")
+			print "here"
+		if button == "lucky":
+			return render_template("result.html",result="lucky")
+
 
 @app.route("/about")
 def about():
